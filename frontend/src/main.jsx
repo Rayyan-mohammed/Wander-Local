30b824439cf9c6cbb225b36c748b386dd576887e
MD Rayyan <rayyan1652@gmail.com>
2026-04-16 09:58:33 +0530

    chore: remove react codebase

--- frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ExperienceDetail from './pages/ExperienceDetail'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ExperienceDetail />
-  </React.StrictMode>,
-)
